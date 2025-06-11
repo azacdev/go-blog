989c54670b2b3c359763f8cca359d40c35c4553d
azacdev <azacdev@gmail.com>
2025-06-11 01:10:48 +0100

    chore: removed assets folder

--- internal/modules/article/request/articles/store-request.go
+++ internal/modules/article/request/articles/store-request.go
@@ -3,4 +3,5 @@ package articles
 type StoreRequest struct {
 	Title   string `form:"title" binding:"required,min=3,max=100"`
 	Content string `form:"content" binding:"required,min=3,max=60000"`
+	// Image   *multipart.FileHeader `form:"image" binding:"required"`
 }
